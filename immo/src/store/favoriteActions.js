5909e2f40b83a1c28c7e5d2a4662bfedbded6a0d
Adrian B <adrian.biedny@student.hogent.be>
2023-06-03 14:41:45 +0200

    removed template redux store

--- immo/src/store/favoriteActions.js
+++
@@ -1,9 +0,0 @@
-export const addFavorite = (id) => ({
-    type: 'ADD_FAVORITE',
-    payload: id,
-  });
-  
-  export const removeFavorite = (id) => ({
-    type: 'REMOVE_FAVORITE',
-    payload: id,
-  });
--- immo/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import favoriteReducer from './favoriteReducer';
-
-const store = createStore(favoriteReducer);
-
-export default store;
